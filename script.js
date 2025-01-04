7430483995af05d061a4e9644b1a7a6f19085167
1F10230198 <s1f10231987@iniad.org>
2025-01-05 01:42:23 +0900

    ナビゲーションバーの追加、その他コメントアウトの追加

--- script.js
+++ script.js
@@ -7,17 +7,53 @@ window.addEventListener('load', () => {
         }, 1000);
     }, 3000);
 
-
     function adjustContentMargin() {
         const sidebar = document.querySelector('.sidebar');
         const content = document.querySelector('.content');
+        const navbar = document.querySelector('.navbar');
+
         if (window.innerWidth > 768) {
-          content.style.marginLeft = sidebar.offsetWidth + 'px';
-        }else{
-           content.style.marginLeft = '0';
+            if (sidebar && sidebar.style.display !== 'none') {
+              content.style.marginLeft = sidebar.offsetWidth + 'px';
+            } else {
+              content.style.marginLeft = '0';
+            }
+             if(navbar) {
+                navbar.style.display = 'none';
+              }
+            if(sidebar) {
+                sidebar.style.display = 'flex';
+              }
+        } else {
+            content.style.marginLeft = '0';
+            if(navbar) {
+                navbar.style.display = 'flex';
+            }
+           if(sidebar) {
+                sidebar.style.display = 'none';
+              }
         }
     }
 
     adjustContentMargin();
     window.addEventListener('resize', adjustContentMargin);
+
+    // worksセクションのフェードイン処理
+    const worksSection = document.querySelector('.works');
+
+    function handleScroll() {
+        const worksSectionTop = worksSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (worksSectionTop < windowHeight * 0.75) {
+            worksSection.classList.add('fade-in');
+            window.removeEventListener('scroll', handleScroll); // 一度だけ実行
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.scrollTo(0, 0);
 });
